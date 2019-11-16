3b0cdb14c43255d322ded81b1c41a3d82e7973d2
Gzoref <gzorf@gmail.com>
2019-11-16 13:21:31 -0500

    Parse

--- parse.c
+++ parse.c
@@ -1,5 +1,14 @@
 #include "shell.h"
 
+
+/**
+ * parse_line - Uses strtok to put args in array
+ *
+ * @line: char pointer
+ *
+ * Return: Double char pointer
+ */
+
 char **parse_line(char *line)
 {
 	int buffer_size = BUFFERSIZE;
@@ -7,7 +16,7 @@ char **parse_line(char *line)
 	char **tokens;
 	char *token;
 
-	tokens = malloc(sizeof(char*) * buffer_size);
+	tokens = malloc(sizeof(char *) * buffer_size);
 
 	if (!tokens)
 	{
@@ -26,12 +35,13 @@ char **parse_line(char *line)
 	if (location >= buffer_size)
 	{
 		buffer_size += BUFFERSIZE;
-		tokens = realloc(tokens, buffer_size * sizeof(char*));
+		tokens = realloc(tokens, buffer_size * sizeof(char *));
 
-		if (!tokens) {
+		if (!tokens)
+		{
 			{
 				perror("hsh: allocation error\n");
-			exit(EXIT_FAILURE);
+				exit(EXIT_FAILURE);
 			}
 		}
 		token = strtok(NULL, DELIMINATOR);
