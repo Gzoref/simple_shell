52f5bb82732b407d9b7e3c86e26fc7c3fd129f11
eNobreg <ezra.nobrega@outlook.com>
2019-11-16 17:48:53 -0500

    update to work with current version of the github repo

--- shell.h
+++ shell.h
@@ -4,18 +4,26 @@
 typedef struct format
 {
 	char *id;
-	int (*f)(char **address)
+	int (*f)(char **address);
 } cmd_fo;
+
+#define BUFFERSIZE 1024
+#define DELIMINATOR " "
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <limits.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int call_cd (char **str);
 int call_exit (char **str);
 int call_env (char **str);
 
-int exec_cmd (char **);
+char **parse_line(char *line);
+int function_filter (char **str, char **env);
+int exec_cmd (char **str, char **env);
+char *read_line(void);
 #endif
