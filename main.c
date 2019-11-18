f778b26b4c13b011d0d034a2bed0b3392c069974
Gzoref <gzoref@gmail.com>
2019-11-18 17:21:05 -0500

    Main 1.4

--- main.c
+++ main.c
@@ -6,14 +6,15 @@ int main(int argc, char **argv, char **env)
 	char *buffer = NULL;
 	char **args;
 	int value = 0;
+	
 	do
 	{
 		write(STDOUT_FILENO, "$ ", 2);
 		buffer = read_line();
 		args = parse_line(buffer);
-		value = function_filter(args, env);
+		value = function_filter(args);
 		free(buffer);
 		free(args);
 	}
-	while(value > 0);	
+	while(value > 0);
 }
