aeda460e4ff4a02e960588ad4c97f5f1699a157c
Gzoref <gzoref@gmail.com>
2019-11-18 16:58:31 -0500

    Filter built-ins

--- filter.c
+++ filter.c
@@ -1,17 +1,41 @@
 #include "shell.h"
 
-int function_filter(char **commands, char **env)
-{
-	cmd_fo format[] = {
-		{"exit", &call_exit},
-		{NULL, NULL}	
-	};
-	
-	int i = 0;
-	
-	for (i = 0; format[i].id != NULL; i++)
-		if (commands[0] == format[i].id)
-			return ((format[i].f)(commands));
 
-	return (exec_cmd(commands, env));
+/**
+ * function_filter - Filters built-ins & sends them to appropriate function
+ *
+ * @commands: Double pointer from command ine
+ *
+ * Return: exec_cmd function
+ */
+
+
+int function_filter(char **commands)
+{
+	int i = 0;
+
+	char *builtin[5];
+
+	builtin[0] = "exit";
+	builtin[1] = "cd";
+	builtin[2] = "help";
+	builtin[3] = NULL;
+
+	while (builtin[i] != NULL)
+	{
+		if (_strcmp(builtin[i], commands[0]) == 0)
+		{
+			switch (i)
+			{
+			case 0:
+				call_exit();
+				break;
+			}
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (exec_cmd(commands, __environ));
 }
