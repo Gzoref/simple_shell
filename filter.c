0a1deaa607fb0d3d0d2319414ba7f169c2d6ba4f
Gzoref <gzoref@gmail.com>
2019-11-19 19:32:30 -0500

    Filte rexit

--- filter.c
+++ filter.c
@@ -4,15 +4,17 @@
 /**
  * function_filter - Filters built-ins & sends them to appropriate function
  *
- * @commands: Double pointer from command ine
+ * @commands: Double pointer from command line
+ *
+ * @env: Environment variable
  *
  * Return: exec_cmd function
  */
 
 
-int function_filter(char **commands)
+int function_filter(char **commands, char **env)
 {
-	int i = 0;
+	int option = 0;
 
 	char *builtin[5];
 
@@ -21,25 +23,23 @@ int function_filter(char **commands)
 	builtin[2] = "help";
 	builtin[3] = NULL;
 
-	while (builtin[i] != NULL)
+	while (builtin[option] != NULL)
 	{
-		printf("Got here 1\n");
-		if (_strcmp(builtin[i], commands[0]) == 0)
+		if (_strcmp(builtin[option], commands[0]) == 0)
 		{
-			printf("Got here 2\n");
-			switch (i)
-			{
-			case 0:
-				printf("Got here 3\n");
-				call_exit();
-				break;
-			case 1:
-				call_cd();
-			default:
-				return (exec_cmd(commands, __environ));
-			}
+			break;
 		}
-		i++;
+		option++;
 	}
-	return (1);
+
+	switch (option)
+	{
+	case 0:
+		printf("Got here 3\n");
+		call_exit();
+		break;
+	default:
+		return (exec_cmd(commands, env));
+	}
+	return (option);
 }
