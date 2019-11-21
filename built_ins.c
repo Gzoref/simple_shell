8a1d27d153aaf540649de02e73dd2dc5f976a200
Gzoref <gzoref@gmail.com>
2019-11-21 18:21:55 -0500

    Add cd

--- built_ins.c
+++ built_ins.c
@@ -1,6 +1,39 @@
 #include "shell.h"
 
-void call_exit (void)
+/**
+ *  call_exit - Exits shell
+ *
+ *  @args: passes in char pointer
+ *
+ *  Return: Integer
+ */
+
+int call_exit(char *args)
 {
 	exit(EXIT_SUCCESS);
+	fflush(stdout);
+}
+
+
+/**
+ *  call_cd - Change directory
+ *
+ *  @args: Passes in char pointer
+ *
+ *  Return: Integer
+ */
+
+int call_cd(char **args)
+{
+
+	if (args[1] == NULL)
+	{
+		perror("cd");
+	}
+	else if (chdir(args[1]) != 0)
+		{
+			perror("hsh");
+		}
+
+	return (1);
 }
