4aaa2ad90ec95cfbfb6b8f5f1d8a2095eb284efc
eNobreg <ezra.nobrega@outlook.com>
2019-11-22 11:17:09 -0500

    updated to work with two types of commands

--- call_exec.c
+++ call_exec.c
@@ -4,18 +4,22 @@
 #include <errno.h>
 
 int find_path(char **enviorment);
+int check_input(char **str, char **env);
 
 int exec_cmd(char **str, char **env)
 {
 	
 	pid_t pid, wpid;
-	int id = 0, i = 0, c = 5, count = 0;
+	int id = 0, i = 0, c = 5, count = 0, flag = 0;
 	char *newstr = malloc(sizeof(char) * BUFFERSIZE), *path = malloc(sizeof(char) * BUFFERSIZE);
 	char *new2 = malloc(sizeof(char) * BUFFERSIZE);
 	char *newp = NULL;
 	
 	memset(new2, '\0', BUFFERSIZE);
-	
+
+	if ((flag = check_input(str, env)) == 1)
+		return(1);
+
 	i = find_path(env);
 	path = strcpy(path, env[i]);
 	count = strlen(path - 5);
@@ -58,7 +62,7 @@ int exec_cmd(char **str, char **env)
 		}
 		while(!WIFSIGNALED(id) && !WIFEXITED(id));
 
-	return (1);		
+	return (1);
 }
 
 
@@ -72,11 +76,11 @@ int find_path(char **env)
 	
 	while (*env)
 	{
-		
+			
 		strcpy(path, env[i]);
 
 		args = strtok(path, "=");
-		if ((id = strcmp(args, "PATH")) == 0)
+		if ((id = _strcmp(args, "PATH")) == 0)
 		{
 			return(i);
 		}
@@ -84,4 +88,52 @@ int find_path(char **env)
 		i++;
 	}
 	return (0);
+}
+
+int check_input(char **str, char **env)
+{
+	pid_t pid, wpid;
+	int id = 0, i = 0;
+	char *sep = "/";
+	char *copy = _strdup(str[0]);
+
+	if ((i = access(str[0], R_OK | X_OK)) == 0)
+	{
+		
+		pid = fork();
+		if (pid == 0)
+		{
+			id = execve(str[0], str, env);
+			if(id == -1)
+			{
+				perror("hsh");
+			}
+			exit(EXIT_SUCCESS);
+		}
+		else if (pid < 0)
+			perror("hsh");
+		else
+		
+			do
+			{
+				wpid = waitpid(pid, &id, WUNTRACED);
+
+			}
+			while(!WIFSIGNALED(id) && !WIFEXITED(id));
+		free(copy);
+		return (1);
+	}
+	else if (copy != NULL)
+	{
+		for (i = 0; copy[i] != '\0'; i++)
+			if(copy[i] == sep[0])
+			{
+				errno = ENOENT;
+				perror("hsh");
+				free(copy);
+				return(1);
+			}
+	}
+	free(copy);
+	return (0);		
 }
