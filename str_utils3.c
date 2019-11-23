296c5fa3f3235fac8ef5ab0da49e86de20846b9f
Gzoref <gzoref@gmail.com>
2019-11-22 22:53:22 -0500

    Exit helper functions

---
+++ str_utils3.c
@@ -0,0 +1,42 @@
+#include "shell.c"
+
+int _exit_atoi(char *str)
+{
+	int result = 0, index = 0, int_max = 2147483647;
+
+	/* Is negative */
+	if (str[index] == '-')
+	{
+		return(-1);
+	}
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		if (_isdigit(str[index]))
+		{
+			result = result * 10 + str[index] - '0';
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+
+	if (result > int_max)
+	{
+		return (-1);
+	}
+
+	else
+		return (result);
+}
+
+
+int _isdigit(char str)
+{
+	if (str >= '0' && str <= '9')
+	{
+		return (1);
+	}
+	return (0);
+}
