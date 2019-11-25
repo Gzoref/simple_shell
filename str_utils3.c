dbac457b7f568e96a03054e32022d98a60e724c9
eNobreg <ezra.nobrega@outlook.com>
2019-11-24 20:00:33 -0500

    added strcpy

--- str_utils3.c
+++ str_utils3.c
@@ -61,3 +61,21 @@ int _isdigit(char str)
 	printf("%d\n", str);
 	return (-1);
 }
+
+/**
+ * _strcpy - Prints a string
+ * @dest: Destination
+ * @src: Source
+ * Return: Always dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
